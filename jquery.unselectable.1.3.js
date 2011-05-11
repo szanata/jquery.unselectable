58678c5a150c8f20c81228f3fc933ffa2fe54819
madeinstefano <made.in.stefano@gmail.com>
2011-05-11 10:43:02 -0300

    1.3 release, now becoming good

---
+++ jquery.unselectable.1.3.js
@@ -0,0 +1,30 @@
+/**
+* jquery.unselectable 1.3
+*
+* This ECMA script disable text selection on HTML elements
+*
+* to go:
+*   
+*   $('any selector').unselectable();
+*
+* p.s.:
+*   don't forget to import the .js file !
+*
+* @license MIT <http://iceon.me/mit.txt>
+* @license GPL <http://iceon.me/gpl.txt>
+* @author Stéfano Stypulkowski <iceon.me>
+* @require jquery 1.4+
+*/
+jQuery.fn.unselectable = function () {
+  
+  $(this).attr('unselectable', 'on')
+    .css({
+      MozUserSelect: 'none',
+      KhtmlUserSelect: 'none',
+      WebkitUserSelect: 'none',
+      userSelect: 'none'
+    })
+    .each(function() { 
+      this.onselectstart = function() { return false; };
+    });
+};
